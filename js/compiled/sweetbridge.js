9abed314cb6d8b79a75b56ab55288bee3d73780f
petrstepanov <stepanovps@gmail.com>
2018-02-02 05:45:59 -0500

    More sections
    1. `How's it all possible` section done
    2. `With Sweetbridge` section completed

--- js/compiled/sweetbridge.js
+++ js/compiled/sweetbridge.js
@@ -14258,3 +14258,22 @@ Object.defineProperty(exports, '__esModule', { value: true });
 })));
 //# sourceMappingURL=bootstrap.js.map
 
+$(document).ready(function() {
+  // Gets the video src from the data-src on each button
+  var $videoSrc;
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+
+  // When the modal is opened autoplay it
+  $('#myModal').on('shown.bs.modal', function (e) {
+    // Set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
+  });
+
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+      // a poor man's stop video
+      $("#video").attr('src',$videoSrc);
+  });
+});
