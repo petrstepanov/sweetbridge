4e24f5d4abbc4458a6711ba966d0ced7d21c94cf
petrstepanov <stepanovps@gmail.com>
2018-02-02 08:41:46 -0500

    Finishing Sections

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
 gulp.task('scripts', function() {
   gulp.src(['./node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
+            './node_modules/swiper/dist/js/swiper.js',
             './js/main.js'
     ])
     .pipe(plumber()) // prevents breaking and has to go first here
@@ -24,6 +25,7 @@ gulp.task('scripts', function() {
 
   gulp.src(['./node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
+            './node_modules/swiper/dist/js/swiper.js',
             './js/main.js'
     ])
     .pipe(plumber()) // prevents breaking and has to go first here
--- js/main.js
+++ js/main.js
@@ -17,3 +17,13 @@ $(document).ready(function() {
       $("#video").attr('src',$videoSrc);
   });
 });
+
+// Initialize swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
